b87e78e2b4790fe67c3ddbf5b5feae92d73ce6a6
Renan Martinez <remam_martinez89@hotmail.com>
2022-07-27 11:35:56 -0300

    modificado base url

--- src/Constants/url.js
+++ src/Constants/url.js
@@ -1,7 +1,1 @@
-// export const BASE_URL = "https://us-central1-missao-newton.cloudfunctions.net/rappi4C"
-
-export const BASE_URL = "https://us-central1-missao-newton.cloudfunctions.net/futureEatsB"
-
-
-
-// 'https://us-central1-missao-newton.cloudfunctions.net/futureEatsB/
+export const BASE_URL = "https://us-central1-missao-newton.cloudfunctions.net/rappi4C"
